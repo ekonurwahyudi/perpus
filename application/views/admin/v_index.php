f5431dc867922d97052407a3a90f4a449aca48ed
Eko Nurwahyudi <generasic@gmail.com>
2020-04-13 16:41:53 +0700

    Membuat Ringkasan Pada Dashboard

--- application/views/admin/v_index.php
+++ application/views/admin/v_index.php
@@ -5,12 +5,58 @@
             <p>Anda telah login sebagai <b><?php echo $this->session->userdata('username');?></b>. </p>
         </div>
     </div>
-    <div class="card">
-        <div class="card-header">
-            Dashboard
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card bg-primary text-white">
+                <div class="card-body">
+                    <h1>
+                        <?php echo $this->m_data->get_data('buku')->num_rows(); ?>
+                        <div class="pull-right">
+                        <i class="fa fa-book"></i>
+                        </div>
+                    </h1>
+                    Jumlah Buku
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            Halo
+        <div class="col-md-3">
+            <div class="card bg-danger text-white">
+                <div class="card-body">
+                    <h1>
+                        <?php echo $this->m_data->get_data('anggota')->num_rows(); ?>
+                        <div class="pull-right">
+                        <i class="fa fa-users"></i>
+                        </div>
+                    </h1>
+                    Jumlah Anggota
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-warning text-white">
+                <div class="card-body">
+                    <h1>
+                        <?php echo $this->m_data->get_data('peminjaman')->num_rows(); ?>
+                        <div class="pull-right">
+                        <i class="fa fa-book"></i>
+                        </div>
+                    </h1>
+                    Jumlah Total Peminjaman
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-primary text-white">
+                <div class="card-body">
+                    <h1>
+                        <?php echo $this->m_data->get_data('petugas')->num_rows(); ?>
+                        <div class="pull-right">
+                        <i class="fa fa-user"></i>
+                        </div>
+                    </h1>
+                    Jumlah Petugas
+                </div>
+            </div>
         </div>
     </div>
 </div>
